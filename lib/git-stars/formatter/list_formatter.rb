9783548743fbdc248f873a6f8777632ec6871406
rochefort <terasawan@gmail.com>
2015-11-11 04:10:45 +0900

    Extract a class method to util.rb

--- lib/git-stars/formatter/list_formatter.rb
+++ lib/git-stars/formatter/list_formatter.rb
@@ -61,24 +61,5 @@ class GitStars
         @columns_sizes[-1] = description_width if description_width >= DEFAULT_COLUMNS_SIZES.last
       end
     end
-
-    # https://github.com/cldwalker/hirb/blob/master/lib/hirb/util.rb#L61-71
-    def detect_terminal_size
-      if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
-        [ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
-      elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM'])) && command_exists?('tput')
-        [`tput cols`.to_i, `tput lines`.to_i]
-      elsif STDIN.tty? && command_exists?('stty')
-        `stty size`.scan(/\d+/).map {  |s| s.to_i }.reverse
-      else
-        nil
-      end
-    rescue
-      nil
-    end
-
-    def command_exists?(command)
-      ENV['PATH'].split(File::PATH_SEPARATOR).any? { |d| File.exist? File.join(d, command) }
-    end
   end
 end
---
+++ lib/git-stars/util.rb
@@ -0,0 +1,24 @@
+class Util
+  class << self
+    # https://github.com/cldwalker/hirb/blob/master/lib/hirb/util.rb#L61-71
+    def detect_terminal_size
+      if (ENV['COLUMNS'] =~ /^\d+$/) && (ENV['LINES'] =~ /^\d+$/)
+        [ENV['COLUMNS'].to_i, ENV['LINES'].to_i]
+      elsif (RUBY_PLATFORM =~ /java/ || (!STDIN.tty? && ENV['TERM'])) && command_exists?('tput')
+        [`tput cols`.to_i, `tput lines`.to_i]
+      elsif STDIN.tty? && command_exists?('stty')
+        `stty size`.scan(/\d+/).map {  |s| s.to_i }.reverse
+      else
+        nil
+      end
+    rescue
+      nil
+    end
+
+    private
+
+    def command_exists?(command)
+      ENV['PATH'].split(File::PATH_SEPARATOR).any? { |d| File.exist? File.join(d, command) }
+    end
+  end
+end
