9765be03f6fa926436647a1ad5e3a0ca0ac0a516
rochefort <terasawan@gmail.com>
2015-12-03 09:56:32 +0900

    Remove needless comments

--- lib/git-stars/cli.rb
+++ lib/git-stars/cli.rb
@@ -48,8 +48,6 @@ class GitStars
       end
     end
 
-    # TODO: verbose option
-    # desc :list, "\033[32m(DEFAULT COMMAND)\e[0m Listing github stars", hide: true
     desc :list, '', hide: true
     option :token,       aliases: '-t', required: false, desc: 'Access token to use when connecting to the Github'
     option :user,        aliases: '-u', required: false, desc: 'User to use when connecting to the Github'
